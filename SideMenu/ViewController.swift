f9a16f755913a4d28e98a0df97cd44f1315c380a
eslam <i52eslam@gmail.com>
2020-05-08 23:30:29 +0200

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Eslam on 5/8/20.
+//  Copyright © 2020 Eslam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
